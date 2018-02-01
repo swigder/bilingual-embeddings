ac57c29781897672ccf63bf4fffac4e5cea6fd66
suri <swigder@gmail.com>
2018-02-01 14:43:56 +0100

    bilingual search proof-of-concept with same limitations as monolingual version.

--- search_engine.py
+++ search_engine.py
@@ -22,23 +22,36 @@ class SearchEngine:
         self.index.build(n_trees=5)
 
     def query_index(self, query):
-        query_vector = self._vectorize(word_tokenize(query))
+        query_vector = self._vectorize(word_tokenize(query), indexing=False)
         result = self.index.get_nns_by_vector(query_vector, n=1)[0]
         return self.documents[result]
 
-    def _vectorize(self, tokens):
+    def _vectorize(self, tokens, indexing=True):
         return np.sum(self.dictionary.word_vectors(tokens=tokens), axis=0)
+
+
+class BilingualSearchEngine(SearchEngine):
+    def __init__(self, dictionary):
+        super().__init__(dictionary=dictionary)
+
+    def _vectorize(self, tokens, indexing=False):
+        return np.sum(self.dictionary.word_vectors(tokens=tokens, reverse=not indexing), axis=0)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Search Engine.')
 
-    parser.add_argument('emb_file', type=str, help='File with embeddings')
+    parser.add_argument('src_emb_file', type=str, help='File with document embeddings')
+    parser.add_argument('tgt_emb_file', type=str, help='File with query embeddings', default=None)
 
     args = parser.parse_args()
 
-    mono_dict = MonolingualDictionary(args.emb_file)
-    search_engine = SearchEngine(dictionary=mono_dict)
+    if args.tgt_emb_file is None:  # monolingual
+        mono_dict = MonolingualDictionary(args.src_emb_file)
+        search_engine = SearchEngine(dictionary=mono_dict)
+    else:  # bilingual
+        bi_dict = BilingualDictionary(args.src_emb_file, args.tgt_emb_file)
+        search_engine = BilingualSearchEngine(dictionary=bi_dict)
 
     print('Type each sentence to index, followed by enter. When done, hit enter twice.')
     sentences = []
