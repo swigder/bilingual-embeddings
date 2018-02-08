596af830bbe03f0955b3c041b9530430a07a0e53
suri <swigder@gmail.com>
2018-02-08 10:00:10 +0100

    fixed bugs in translation dictionary. added functionality to switch language direction on command line

--- dictionary.py
+++ dictionary.py
@@ -7,6 +7,9 @@ from gensim.models.keyedvectors import EuclideanKeyedVectors
 
 
 class Dictionary:
+    def word_vector(self, token):
+        return self.word_vectors([token])[0]
+
     def word_vectors(self, tokens):
         pass
 
@@ -14,10 +17,11 @@ class Dictionary:
         pass
 
 
-class MonolingualDictionary:
-    def __init__(self, emb_file):
+class MonolingualDictionary(Dictionary):
+    def __init__(self, emb_file, language=None):
         self.emb = EuclideanKeyedVectors.load_word2vec_format(emb_file, binary=False)
         self.vector_dimensionality = self.emb.vector_size
+        self.language = language if language is not None else os.path.basename(emb_file).split('.')[0]
 
     def safe_word_vector(self, token):
         if not self.emb.__contains__(token):
@@ -48,13 +52,13 @@ class BilingualDictionary(Dictionary):
 
     def translate(self, src_word, topn=1, reverse=False):
         src_emb, tgt_emb = self._embeddings(reverse)
-        src_vector = src_emb.word_vectors(src_word)[0]
+        src_vector = src_emb.word_vector(src_word)
         return tgt_emb.synonyms(src_vector, topn=topn, vector=True)
 
     def synonyms(self, src_word, topn=1, reverse=False):
         src_emb, _ = self._embeddings(reverse)
         return src_emb.synonyms(src_word, topn=topn)
-    
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Bilingual Dictionary.')
@@ -69,6 +73,17 @@ if __name__ == "__main__":
 
     bi_dict = BilingualDictionary(args.src_emb_file, args.tgt_emb_file)
 
+    reverse = False
+
+    def language_direction():
+        return ('{1} -> {0}' if reverse else '{0} -> {1}').format(bi_dict.src_emb.language, bi_dict.tgt_emb.language)
+
+    print('Loaded dictionaries. Enter a word and hit enter to translate. Hit enter twice to reverse languages.')
+    print(language_direction())
     while True:
         word = input(">> ")
-        print(bi_dict.translate(word, topn=args.top_n))
+        if not word:
+            reverse = not reverse
+            print('Reversed, new direction:', language_direction())
+            continue
+        print(bi_dict.translate(word, topn=args.top_n, reverse=reverse))
