a0624d85da0b9f3c38e34f5e4dcd81fe3f1480c2
suri <swigder@gmail.com>
2018-02-09 16:12:25 +0100

    simplified the calling of ir readers by providing default files

--- ir_data_reader.py
+++ ir_data_reader.py
@@ -1,7 +1,18 @@
 import argparse
 
+import os
+
+
+def dir_appender(dir_location):
+    return lambda file: os.path.join(dir_location, file)
+
 
 class IrDataReader:
+    def __init__(self, doc_file, query_file, relevance_file):
+        self.doc_file = doc_file
+        self.query_file = query_file
+        self.relevance_file = relevance_file
+
     def _read_file(self, file, extract_id_fn):
         items = {}
         current_id = ''
@@ -24,9 +35,22 @@ class IrDataReader:
 
         return items
 
-    def _read_relevance(self, file):
+    def read_documents_queries_relevance(self):
+        return {
+            'documents': self.read_documents(),
+            'queries': self.read_queries(),
+            'relevance': self.read_relevance_judgments(),
+        }
+
+    def read_documents(self):
+        return self._read_file(self.doc_file, self.extract_doc_id)
+
+    def read_queries(self):
+        return self._read_file(self.query_file, self.extract_query_id)
+
+    def read_relevance_judgments(self):
         items = {}
-        with open(file) as f:
+        with open(self.relevance_file) as f:
             for line in f:
                 if not line.strip():
                     continue
@@ -35,25 +59,6 @@ class IrDataReader:
                     items[query_id] = []
                 items[query_id] += doc_ids
         return items
-
-    def read(self, documents=None, queries=None, relevance=None):
-        ret = []
-        if documents:
-            ret.append(self.read_documents(documents))
-        if queries:
-            ret.append(self.read_queries(queries))
-        if relevance:
-            ret.append(self.read_relevance_judgments(relevance))
-        return ret
-
-    def read_documents(self, file):
-        return self._read_file(file, self.extract_doc_id)
-
-    def read_queries(self, file):
-        return self._read_file(file, self.extract_query_id)
-
-    def read_relevance_judgments(self, file):
-        return self._read_relevance(file)
 
     def extract_doc_id(self, line):
         pass
@@ -72,7 +77,9 @@ class IrDataReader:
 
 
 class TimeReader(IrDataReader):
-    def __init__(self):
+    def __init__(self, data_dir):
+        f = dir_appender(data_dir)
+        IrDataReader.__init__(self, doc_file=f('TIME.ALL'), query_file=f('TIME.QUE'), relevance_file=f('TIME.REL'))
         self.id = 0
 
     def extract_doc_id(self, line):
@@ -92,11 +99,15 @@ class TimeReader(IrDataReader):
 
 
 class AdiReader(IrDataReader):
+    def __init__(self, data_dir):
+        f = dir_appender(data_dir)
+        IrDataReader.__init__(self, doc_file=f('ADI.ALL'), query_file=f('ADI.QRY'), relevance_file=f('ADI.REL'))
+
     @staticmethod
     def extract_id(line):
         if not line.startswith('.I'):
             return None
-        return line.split()[1]
+        return int(line.split()[1])
 
     def extract_doc_id(self, line):
         return self.extract_id(line)
@@ -115,29 +126,23 @@ class AdiReader(IrDataReader):
 readers = {'time': TimeReader, 'adi': AdiReader}
 
 
+def print_description(items, description):
+    print()
+    print('Read {} items in {}. Example items:'.format(len(items), description))
+    keys = list(items.keys())
+    print(keys[0], ':', items[keys[0]][:300])
+    print(keys[1], ':', items[keys[1]][:300])
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='IR data reader.')
 
     parser.add_argument('dir', type=str, help='Directory with files')
-    parser.add_argument('-d', '--documents', type=str, help='Documents file', required=False)
-    parser.add_argument('-q', '--queries', type=str, help='Queries file', required=False)
-    parser.add_argument('-r', '--relevance', type=str, help='Relevance file', required=False)
     parser.add_argument('-t', '--type', choices=readers.keys(), default='time')
 
     args = parser.parse_args()
 
-    reader = readers[args.type]
+    reader = readers[args.type](os.path.join(args.dir, args.type))
 
-    def print_description(items, file):
-        print()
-        print('Read {} items in file {}. Example items:'.format(len(items), file))
-        keys = list(items.keys())
-        print('{}: {}'.format(keys[0], items[keys[0]][:300]))
-        print('{}: {}'.format(keys[1], items[keys[1]][:300]))
-
-    if args.documents:
-        print_description(reader().read_documents(args.dir + args.documents), args.documents)
-    if args.queries:
-        print_description(reader().read_queries(args.dir + args.queries), args.queries)
-    if args.relevance:
-        print_description(reader().read_relevance_judgments(args.dir + args.relevance), args.relevance)
+    for name, item in reader.read_documents_queries_relevance().items():
+        print_description(item, name)
--- run_tests.py
+++ run_tests.py
@@ -1,30 +1,64 @@
+import argparse
+import os
+
 from dictionary import MonolingualDictionary
-from ir_data_reader import TimeReader
+from ir_data_reader import readers
 from search_engine import SearchEngine
 
 
-time_dir = '/Users/xx/Documents/school/kth/thesis/ir-datasets/time/'
-doc_file = time_dir + 'TIME.ALL'
-query_file = time_dir + 'TIME.QUE'
-relevance_file = time_dir + 'TIME.REL'
-embed_file = '/Users/xx/Downloads/MUSE-master/trained/vectors-en.txt'
+def precision_recall(expected, actual):
+    true_positives = sum([1 for item in expected if item in actual])
+    return true_positives / len(actual), true_positives / len(expected)
 
-documents, queries, relevance = TimeReader().read(documents=doc_file, queries=query_file, relevance=relevance_file)
 
-mono_dict = MonolingualDictionary(emb_file=embed_file)
-search_engine = SearchEngine(dictionary=mono_dict)
+def read_data(data_dir, doc_file, query_file, relevance_file, reader):
+    f = lambda file: os.path.join(data_dir, file)
 
-search_engine.index_documents(documents.values())
-n_results = 10
-correct = 0
-for i, query in queries.items():
-    print()
-    print(i, query, relevance[i])
-    results = search_engine.query_index(query, n_results=n_results)
-    for distance, result in results:
-        result_i = list(documents.keys())[list(documents.values()).index(result)]
-        print(result_i, distance, result[:300])
-        if result_i in relevance[i]:
-            print('Correct!')
-            correct += 1
-print(correct, len(queries)*n_results)
+    return reader.read(f(doc_file), f(query_file), f(relevance_file))
+
+
+def test_search_engine(search_engine, documents, queries, relevance, n=5, verbose=False):
+    search_engine.index_documents(documents.values())
+    total_precision, total_recall = 0, 0
+    for i, query in queries.items():
+        results = search_engine.query_index(query, n_results=n)
+        results_i = []
+        if verbose:
+            print()
+            print(i, query, relevance[i])
+        for distance, result in results:
+            result_i = list(documents.keys())[list(documents.values()).index(result)]
+            results_i.append(result_i)
+            if verbose:
+                print(result_i, distance, result[:300])
+                if result_i in relevance[i]:
+                    print('Correct!')
+        precision, recall = precision_recall(expected=relevance[i], actual=results_i)
+        total_precision += precision
+        total_recall += recall
+    print(total_precision / len(queries), total_recall / len(queries))
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='IR data reader.')
+
+    parser.add_argument('ir_dir', type=str, help='Directory with IR files', nargs='?')
+    parser.add_argument('embed', type=str, help='Embedding file', nargs='?')
+    parser.add_argument('-t', '--type', choices=readers.keys(), default='time')
+    parser.add_argument('-n', '--number_results', type=int, default=5)
+    parser.add_argument('-v', '--verbose', type=bool, default=False)
+
+    args = parser.parse_args()
+
+    if not args.ir_dir:
+        args.ir_dir = '/Users/xx/Documents/school/kth/thesis/ir-datasets/'
+    if not args.embed:
+        args.embed = '/Users/xx/Downloads/MUSE-master/trained/vectors-en.txt'
+
+    reader = readers[args.type](os.path.join(args.ir_dir, args.type))
+
+    mono_dict = MonolingualDictionary(emb_file=args.embed)
+    test_search_engine(SearchEngine(dictionary=mono_dict),
+                       **reader.read_documents_queries_relevance(),
+                       n=args.number_results,
+                       verbose=args.verbose)
