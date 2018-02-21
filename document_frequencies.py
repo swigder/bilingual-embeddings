5f50921292923d1f84be0964f682be8b48720080
suri <swigder@gmail.com>
2018-02-21 16:20:36 +0100

    faster document frequencies by not processing low-frequency words until the end. testing different configurations for df.

--- document_frequencies.py
+++ document_frequencies.py
@@ -31,7 +31,10 @@ def process_article(article_text, dfs):
 
 
 def compute_dfs(in_file_path, out_file_path, min_count=0, max_docs=-1, save_rate=10000):
-    os.remove(out_file_path) # so don't merge old data
+    try:
+        os.remove(out_file_path) # so don't merge old data
+    except FileNotFoundError:
+        pass
 
     dfs = defaultdict(int)
     total_docs = 0
@@ -46,7 +49,7 @@ def compute_dfs(in_file_path, out_file_path, min_count=0, max_docs=-1, save_rate
                     print('Processing', total_docs, line)
                 if total_docs % save_rate == 0:
                     print('Saving at', total_docs, line)
-                    write_to_file(out_file_path, dfs, num_docs=total_docs-offset, min_count=min_count)
+                    write_to_file(out_file_path, dfs, num_docs=total_docs-offset, merge=True)
                     dfs.clear()
                     sub_tokens.clear()
                     offset = total_docs
@@ -57,25 +60,64 @@ def compute_dfs(in_file_path, out_file_path, min_count=0, max_docs=-1, save_rate
             current_article += line.strip() + ' '
         process_article(current_article, dfs)
 
-    write_to_file(out_file_path, dfs, num_docs=total_docs-offset, min_count=min_count)
+    write_to_file(out_file_path, dfs, num_docs=total_docs-offset, merge=True)
+
+    dfs, num_docs = read_dfs(out_file_path)
+    merge_low_df_files(out_file_path, dfs)
+    write_to_file(out_file_path, dfs, num_docs=num_docs, min_count=min_count, merge=False)
 
 
-def write_to_file(file_path, dfs, num_docs, min_count):
+def write_low_df_to_files(file_path, dfs):
+    max_count = 5
+    to_write = {i+1: [] for i in range(max_count)}
+    for token, count in dfs.items():
+        if count <= max_count:
+            to_write[count].append(token)
+    path, ext = os.path.splitext(file_path)
+    for count, tokens in to_write.items():
+        count_path = '{}-low-df-{}{}'.format(path, count, ext)
+        with open(count_path, 'a') as file:
+            for token in tokens:
+                del dfs[token]
+                file.write(token + '\n')
+
+
+def merge_low_df_files(file_path, dfs):
+    path, ext = os.path.splitext(file_path)
+    for count in range(1, 6):
+        count_path = '{}-low-df-{}{}'.format(path, count, ext)
+        try:
+            with open(count_path, 'r') as file:
+                for line in file:
+                    dfs[line.strip()] += count
+        except FileNotFoundError:
+            pass
+
+
+def delete_low_dfs(dfs, min_count):
+    to_delete = set()
+    for token, count in dfs.items():
+        if count < min_count:
+            to_delete.add(token)
+    for token in to_delete:
+        del dfs[token]
+
+
+def write_to_file(file_path, dfs, num_docs, min_count=0, merge=False):
+    if min_count == 0:
+        write_low_df_to_files(file_path, dfs)
+
+    if merge:
+        try:
+            dfs_2, num_docs_2 = read_dfs(file_path)
+            num_docs += num_docs_2
+            for k, v in dfs_2.items():
+                dfs[k] += v
+        except FileNotFoundError:
+            pass  # nothing to merge
+
     if min_count > 0:
-        to_delete = set()
-        for token, count in dfs.items():
-            if count < min_count:
-                to_delete.add(token)
-        for token in to_delete:
-            del dfs[token]
-
-    try:
-        dfs_2, num_docs_2 = read_dfs(file_path)
-        num_docs += num_docs_2
-        for k, v in dfs_2.items():
-            dfs[k] += v
-    except FileNotFoundError:
-        pass  # nothing to merge
+        delete_low_dfs(dfs, min_count)
 
     with open(file_path, 'w') as out_file:
         out_file.write('{} {}\n'.format(num_docs, len(dfs)))
@@ -93,6 +135,11 @@ def read_dfs(file_name):
             dfs[token] = count
     assert vocab_size == len(dfs)
     return dfs, num_docs
+
+
+def remove_low_dfs_from_file(input_file_name, output_file_name, min_count):
+    dfs, num_docs = read_dfs(input_file_name)
+    write_to_file(output_file_name, dfs, num_docs=num_docs, min_count=min_count)
 
 
 if __name__ == "__main__":
--- run_tests.py
+++ run_tests.py
@@ -1,7 +1,13 @@
 import argparse
+import itertools
+import operator
 import os
 
 from collections import namedtuple
+
+from math import sqrt
+
+import numpy as np
 
 from dictionary import MonolingualDictionary
 from ir_data_reader import readers, sub_collection
@@ -10,6 +16,10 @@ from baseline import CosineSimilaritySearchEngine
 
 
 PrecisionRecall = namedtuple('PrecisionRecall', ['precision', 'recall'])
+
+
+def f1_score(precision, recall):
+    return 2 * precision * recall / (precision + recall)
 
 
 def precision_recall(expected, actual):
@@ -40,7 +50,7 @@ def query_result(search_engine, i, query, expected, documents, n=5, verbose=Fals
     return PrecisionRecall(precision, recall)
 
 
-def compare_search_engines(search_engine, baseline, collection, n=5):
+def compare_search_engines(search_engine, baseline, collection, n=5, print_details=True):
     total_precision, total_recall = 0, 0
     base_precision, base_recall = 0, 0
     doc_ids = {doc_text: doc_id for doc_id, doc_text in collection.documents.items()}
@@ -56,7 +66,7 @@ def compare_search_engines(search_engine, baseline, collection, n=5):
         base_precision += base_pr.precision
         base_recall += base_pr.recall
 
-        if engine_pr.precision < base_pr.precision or engine_pr.recall < base_pr.recall:
+        if print_details and engine_pr.precision < base_pr.precision or engine_pr.recall < base_pr.recall:
             query_result(search_engine, i, query, expected, doc_ids, n, verbose=True)
             print('System: {:.4f} / {:.4f}'.format(engine_pr.precision, engine_pr.recall))
             query_result(baseline, i, query, expected, doc_ids, n, verbose=True)
@@ -74,13 +84,49 @@ def test_search_engine(search_engine, collection, n=5, verbose=False):
     doc_ids = {doc_text: doc_id for doc_id, doc_text in collection.documents.items()}
     for i, query in collection.queries.items():
         expected = collection.relevance[i]
-        pr = query_result(search_engine, i, query, expected, doc_ids, n, verbose=True)
+        pr = query_result(search_engine, i, query, expected, doc_ids, n, verbose=verbose)
         total_precision += pr.precision
         total_recall += pr.recall
     if verbose:
         print()
-    print('Precision / Recall: {:.4f} / {:.4f}'.format(total_precision / len(collection.queries),
-                                                       total_recall / len(collection.queries)))
+    precision, recall = total_precision / len(collection.queries), total_recall / len(collection.queries)
+    # print('Precision / Recall: {:.4f} / {:.4f}'.format(precision, recall))
+    return precision, recall
+
+
+def compare_df_options():
+    df_cutoffs = [.8]
+    smoothing_fns = {
+        'none': lambda num_docs: 0,
+        'one': lambda num_docs: 1,
+        'sqrt': lambda num_docs: int(sqrt(num_docs)),
+    }
+    default_df_fns = {
+        'zero': lambda dfs, smoothing: 0,
+        'min': lambda dfs, smoothing: np.min(dfs),
+        'avg': lambda dfs, smoothing: np.average(dfs),
+        'avg-minus-smoothing': lambda dfs, smoothing: np.average(dfs) - smoothing,
+        'smoothing': lambda dfs, smoothing: max(smoothing, 1)
+    }
+    percentiles = [None, 5, 100]
+    df_option_options = [df_cutoffs, smoothing_fns.items(), default_df_fns.items(), percentiles]
+    results = {}
+    for df_file in [None, '/Users/xx/Downloads/wiki-df-100.txt']:
+        for current_options in itertools.product(*df_option_options):
+            df_cutoff, smoothing_fn, default_df_fn, percentile = current_options
+            if smoothing_fn[0] == 'none' and default_df_fn[0] == 'avg-minus-smoothing':
+                continue
+            df_config = {'df_cutoff': df_cutoff,
+                         'smoothing_fn': smoothing_fn[1],
+                         'default_df_fn': default_df_fn[1],
+                         'percentiles': percentile}
+            search_engine = EmbeddingSearchEngine(dictionary=mono_dict, df_file=df_file, df_options=df_config)
+            search_engine.index_documents(ir_collection.documents.values())
+            precision, recall = test_search_engine(search_engine, ir_collection, verbose=False)
+            key = df_file, df_cutoff, smoothing_fn[0], default_df_fn[0], percentile
+            results[key] = f1_score(precision, recall)
+    for k, v in sorted(results.items(), key=operator.itemgetter(1), reverse=True):
+        print('{} {:.4f}'.format(k, v))
 
 
 def sub_query(query_i, n=None, search_engine=None):
@@ -108,6 +154,7 @@ if __name__ == "__main__":
     parser.add_argument('-v', '--verbose', action='store_true')
     parser.add_argument('-b', '--baseline', action='store_true')
     parser.add_argument('-c', '--compare', action='store_true')
+    parser.add_argument('-d', '--df_file', type=str, help='File with precomputed df', default=None)
 
     args = parser.parse_args()
 
@@ -116,19 +163,6 @@ if __name__ == "__main__":
     if not args.embed:
         args.embed = '/Users/xx/Downloads/MUSE-master/trained/vectors-en.txt'
         # args.embed = '/Users/xx/Downloads/GoogleNews-vectors-negative300.bin.gz'
-    # While processing, articles are tokenized using the much faster 'split'.
-    # Here, we tokenize the tokens using a proper tokenizer.
-    print('Preprocessed tokens:', len(dfs))
-    to_update = {}
-    for super_token, count in dfs.items():
-        tokens = [super_token] if super_token.isalpha() else word_tokenize(super_token)
-        if len(tokens) > 1:
-            to_update[super_token] = (tokens, count)
-    print('Updating:', len(to_update))
-    for super_token, (sub_tokens, count) in to_update.items():
-        del dfs[super_token]
-        for sub_token in sub_tokens:
-            dfs[sub_token] += count
 
     reader = readers[args.type](os.path.join(args.ir_dir, args.type))
     ir_collection = reader.read_documents_queries_relevance()
@@ -137,16 +171,18 @@ if __name__ == "__main__":
 
     if args.compare or not args.baseline:
         mono_dict = MonolingualDictionary(emb_file=args.embed)
-        search_engines['Embedding'] = EmbeddingSearchEngine(dictionary=mono_dict)
+        search_engines['Embedding'] = EmbeddingSearchEngine(dictionary=mono_dict, df_file=args.df_file)
     if args.compare or args.baseline:
         search_engines['Baseline'] = CosineSimilaritySearchEngine()
 
-    for engine in search_engines.values():
-        engine.index_documents(ir_collection.documents.values())
+    # for engine in search_engines.values():
+    #     engine.index_documents(ir_collection.documents.values())
+    #
+    # if args.compare:
+    #     compare_search_engines(search_engines['Embedding'], search_engines['Baseline'],
+    #                            ir_collection, n=args.number_results)
+    # else:
+    #     engine = list(search_engines.values())[0]
+    #     test_search_engine(engine, ir_collection, n=args.number_results, verbose=args.verbose)
 
-    if args.compare:
-        compare_search_engines(search_engines['Embedding'], search_engines['Baseline'],
-                               ir_collection, n=args.number_results)
-    else:
-        engine = list(search_engines.values())[0]
-        test_search_engine(engine, ir_collection, n=args.number_results, verbose=args.verbose)
+    compare_df_options()
