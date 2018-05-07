bb6e215234bc58df95baf6dd43b9c20d12e654a4
suri <swigder@gmail.com>
2018-05-07 17:03:31 +0200

    adding ability to merge files

--- tools/wikipedia.py
+++ tools/wikipedia.py
@@ -76,18 +76,7 @@ def get_plain_text(page):
     return plaintext.replace('=', ' ').replace('\n', ' ')
 
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Get category from wikipedia.')
-
-    parser.add_argument('dir', type=str, help='directory to write to')
-    parser.add_argument('-l', '--lang', choices=['en', 'sv'], default='en', help='language')
-    parser.add_argument('-i', '--ignore', type=str, nargs='?', help='file containing pages to ignore')
-    parser.add_argument('-p', '--pages', type=str, nargs='?', help='file containing pages to read')
-    parser.add_argument('-c', '--category', type=str, nargs='?', help='category')
-    parser.add_argument('-d', '--depth', type=int, default=4, help='depth of category tree to read')
-
-    args = parser.parse_args()
-
+def download_wikipedia(args):
     assert args.category or args.pages
 
     wiki_url = 'https://{}.wikipedia.org/w/api.php'.format(args.lang)
@@ -133,5 +122,61 @@ if __name__ == '__main__':
             if page in processed_pages:
                 continue
             f.write(page + '\n' + get_plain_text(page) + '\n\n')
+
+
+def merge(args):
+    all_pages = []
+    with open(os.path.join(args.to_dir, 'pages.txt'), 'r') as f:
+        for line in f:
+            all_pages.append(line.strip())
+
+    processed_pages = []
+    with open(os.path.join(args.to_dir, 'contents.txt'), 'r') as f:
+        previous_is_blank = True
+        for line in f:
+            line = line.strip()
+            if previous_is_blank and line:
+                processed_pages.append(line)
+            previous_is_blank = not bool(line)
+
+    with open(os.path.join(args.to_dir, 'contents.txt'), 'a') as to_file:
+        with open(os.path.join(args.from_dir, 'contents.txt'), 'r') as from_file:
+            previous_is_blank = True
+            add_current_line = False
+            for line in from_file:
+                line = line.strip()
+                if previous_is_blank and line in all_pages and line not in processed_pages:
+                    to_file.write(line + '\n')
+                    add_current_line = True
+                elif add_current_line:
+                    if line:
+                        to_file.write(line + '\n')
+                    to_file.write('\n')
+                    add_current_line = False
+                previous_is_blank = not bool(line)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Get category from wikipedia.')
+
+    subparsers = parser.add_subparsers()
+
+    parser_download = subparsers.add_parser('dl')
+    parser_download.add_argument('dir', type=str, help='directory to write to')
+    parser_download.add_argument('-l', '--lang', choices=['en', 'sv'], default='en', help='language')
+    parser_download.add_argument('-i', '--ignore', type=str, nargs='?', help='file containing pages to ignore')
+    parser_download.add_argument('-p', '--pages', type=str, nargs='?', help='file containing pages to read')
+    parser_download.add_argument('-c', '--category', type=str, nargs='?', help='category')
+    parser_download.add_argument('-d', '--depth', type=int, default=4, help='depth of category tree to read')
+    parser_download.set_defaults(func=download_wikipedia)
+
+    parser_merge = subparsers.add_parser('merge')
+    parser_merge.add_argument('to_dir', type=str, help='directory to merge into')
+    parser_merge.add_argument('from_dir', type=str, help='directory to merge from')
+    parser_merge.set_defaults(func=merge)
+
+    args = parser.parse_args()
+
+    args.func(args)
 
 
