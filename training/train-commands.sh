5fcbc5ff7fa2e8fe93b252e8ce1ceb10ffe9359f
suri <swigder@gmail.com>
2018-05-08 10:24:30 +0200

    train commands with cl params rather than new script for each experiment

--- training/train-commands.sh
+++ training/train-commands.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 
+experiment_name=$1
+experiment_options=$2
+training_options=$3 # put flag for experiment last, don't include output dir
+
 thesis_home=~/bivec
-output_dir_top=${thesis_home}/embed/dim-only
+output_dir_top=${thesis_home}/embed/${experiment_name}
+mkdir ${output_dir_top}
 
 iterations='1 2 3'
 collections='adi time ohsu-trec'
-dims='50 100 200 300'
 
 for iteration in ${iterations}
 do
@@ -13,9 +17,9 @@ output_dir=${output_dir_top}/${iteration}
 mkdir ${output_dir}
 for collection in ${collections}
 do
-for dim in ${dims}
+for experiment_option in ${experiment_options}
 do
-bash code/train.sh -c ${collection} -d ${dim} -o ${output_dir}
+bash code/train.sh -c ${collection} ${training_options} ${experiment_option} -o ${output_dir}
 done
 done
 done
