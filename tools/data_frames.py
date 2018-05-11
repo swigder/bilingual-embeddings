31e9ae849504dc2802e94d058b7ad26aa6b7de67
suri <swigder@gmail.com>
2018-05-11 09:42:23 +0200

    adding parsing of file directories to map mapping to hyperparameters

--- tools/data_frames.py
+++ tools/data_frames.py
@@ -19,3 +19,24 @@ def load_change_show(path, from_col, to_col):
     pd.set_option('precision', 4)
     print(df.to_latex())
     return df
+
+
+def map_files_to_df_components(file_string):
+    mapping = {}
+    for line in file_string.split('\n'):
+        if not line:
+            continue
+        file = line.split()[-1]
+        parts = file.split('/')
+        mapping[parts[-2]] = parts[-4]
+
+    df = pd.DataFrame(index=mapping.keys(), columns=['sub', 'win', 'epoch', 'min'])
+    for k, v in mapping.items():
+        split = v.split('-')
+        d = {}
+        for i, name in enumerate(split):
+            if name in df.columns:
+                d[name] = split[i + 1]
+        df.loc[k] = d
+
+    return df
