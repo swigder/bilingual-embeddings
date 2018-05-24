849971385af61929189fe14ce1ca7fa72bd5fed2
suri <swigder@gmail.com>
2018-05-24 15:52:18 +0200

    option to average or take nth-highest value

--- tools/data_frames.py
+++ tools/data_frames.py
@@ -1,5 +1,6 @@
 import glob
 import os
+from collections import defaultdict
 
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -8,12 +9,14 @@ import numpy as np
 from matplotlib import gridspec
 
 
+SCORE = 'MAP@10'
+
 attributes = ['sub', 'win', 'epochs', 'norm', 'subword', 'pretrained']
 nice_names = {'sub': 'Minimum subword length',
               'win': 'Window size',
               'epochs': 'Epochs',
               'norm': 'Normalize length',
-              'subword': 'Use subword data for OOV',
+              'subword': 'Use subword for OOV',
               'pretrained': 'Model type'}
 baseline_columns = {'sub': 'No',
                     'win': 5,
@@ -49,14 +52,15 @@ def load_change_show(path, from_col, to_col):
     return df
 
 
-def map_files_to_df_components(file_string):
+def map_files_to_df_components(file_path):
     mapping = {}
-    for line in file_string.split('\n'):
-        if not line:
-            continue
-        file = line.split()[-1]
-        parts = file.split('/')
-        mapping[parts[-2]] = parts[-4]
+    with open(file_path) as f:
+        for line in f:
+            if not line:
+                continue
+            file = line.split()[-1]
+            parts = file.split('/')
+            mapping[parts[-2]] = parts[-4]
 
     df = pd.DataFrame(index=mapping.keys(), columns=['collection', 'sub', 'win', 'epoch', 'min'])
     for k, v in mapping.items():
@@ -81,7 +85,7 @@ def read_all_pickles_in_dirs(paths):
 def read_all_pickles_in_dir(path):
     assert os.path.isdir(path)
     df = pd.concat([read_grid_pickle(file) for file in glob.glob(os.path.join(path, '*.pkl'))], axis=0)
-    return df.drop_duplicates(df.columns.difference(['MAP@10']))
+    return df.drop_duplicates(df.columns.difference([SCORE]))
 
 
 def read_grid_pickle(path):
@@ -129,86 +133,99 @@ def unpickle_multiple(prefix, files):
     return pd.concat([pd.read_pickle(os.path.join(prefix, file)) for file in files]).drop_duplicates()
 
 
-def get_results(file_string, prefix, files):
-    r = pd.concat([map_files_to_df_components(file_string), unpickle_multiple(prefix, files)], axis=1)
+def get_results(file_path, prefix, files):
+    r = pd.concat([map_files_to_df_components(file_path), unpickle_multiple(prefix, files)], axis=1)
     return r.apply(pd.to_numeric, errors='ignore')
 
 
 def get_max_map(df):
-    return df.loc[df.groupby('collection')['MAP@10'].idxmax()]
+    return df.loc[df.groupby('collection')[SCORE].idxmax()]
 
 
-def two_2_map(df, row, col):
-    return df.groupby([row, col])['MAP@10'].mean().unstack()
+def group_scores(df, groupby, average, n=2):
+    if average:
+        return df.groupby(groupby)[SCORE].mean().unstack()
+    else:
+        return df.sort_values([SCORE], ascending=False).groupby(groupby).head(n).groupby(
+            groupby).min()[SCORE].unstack()
 
 
-def parameter_interaction(df):
+def plot_per_collection(df, columns, df_function, suptitle, share_x=False, column_labels=None):
+    if not column_labels:
+        column_labels = {column: column for column in columns}
+
     sns.set()
 
-    for attribute in attributes:
+    fig = plt.figure(figsize=(15, 9))
+    fig.suptitle(suptitle)
+    outer = gridspec.GridSpec(3, 1, wspace=0.1, hspace=0.1)
+
+    collection_groups = df.groupby('collection')
+    for i, collection in enumerate(collection_groups.groups):
+        collection_df = collection_groups.get_group(collection)
+
+        inner = gridspec.GridSpecFromSubplotSpec(1, len(columns), subplot_spec=outer[i], wspace=0.1, hspace=0.1)
+        axes = np.empty(shape=(1, len(columns)), dtype=object)
+        for j in range(len(columns)):
+            sharex_param = {'sharex': axes[0, 0]} if share_x else {}
+            ax = plt.Subplot(fig, inner[j], **sharex_param, sharey=axes[0, 0])
+            fig.add_subplot(ax)
+            axes[0, j] = ax
+
+        for j, column in enumerate(columns):
+            grid = df_function(collection_df, column)
+            ax = axes[0, j]
+            grid.plot(ax=ax)
+            values = grid.index.tolist()
+            if ax.is_last_row():
+                ax.set_xticks(values if all(type(x) is int for x in values) else range(len(values)))
+                ax.set_xticklabels(values)
+                if share_x and j != len(columns) // 2:
+                    ax.set_xlabel('')
+                else:
+                    ax.set_xlabel(column_labels[column])
+            else:
+                plt.setp(ax.get_xticklabels(), visible=False)
+
+            if ax.is_first_col():
+                label = collection if i != 1 else 'MAP@10\n\n' + collection
+                ax.set_ylabel(label)
+            else:
+                plt.setp(ax.get_yticklabels(), visible=False)
+
+    fig.tight_layout()
+    fig.show()
+
+
+def parameter_interaction(df, options, parameters=attributes):
+    for attribute in parameters:
         others = [a for a in attributes if a != attribute]
 
-        fig = plt.figure(figsize=(12, 12))
-        fig.suptitle(nice_names[attribute])
-        outer = gridspec.GridSpec(3, 1, wspace=0.1, hspace=0.1)
+        def df_function(collection_df, other):
+            return group_scores(collection_df, [attribute, other], **options)
 
-        collection_groups = df.groupby('collection')
-        for i, collection in enumerate(collection_groups.groups):
-            collection_df = collection_groups.get_group(collection)
-
-            inner = gridspec.GridSpecFromSubplotSpec(1, 5, subplot_spec=outer[i], wspace=0.1, hspace=0.1)
-            axes = np.empty(shape=(1, 5), dtype=object)
-            for j in range(len(others)):
-                ax = plt.Subplot(fig, inner[j], sharex=axes[0, 0], sharey=axes[0, 0])
-                fig.add_subplot(ax)
-                axes[0, j] = ax
-
-            for j, other in enumerate(others):
-                grid = two_2_map(collection_df, attribute, other)
-                ax = axes[0, j]
-                grid.plot(ax=ax)
-                values = grid.index.tolist()
-                if ax.is_last_row():
-                    ax.set_xticks(values if all(type(x) is int for x in values) else range(len(values)))
-                    ax.set_xticklabels(values)
-                    if i != 2 or j != 2:
-                        ax.set_xlabel('')
-                    else:
-                        ax.set_xlabel(nice_names[attribute])
-                else:
-                    plt.setp(ax.get_xticklabels(), visible=False)
-
-                if ax.is_first_col():
-                    label = collection if i != 1 else 'MAP@10\n\n' + collection
-                    ax.set_ylabel(label)
-                else:
-                    plt.setp(ax.get_yticklabels(), visible=False)
-
-        fig.tight_layout()
-        fig.show()
+        plot_per_collection(df, others, df_function, nice_names[attribute],
+                            share_x=True, column_labels=defaultdict(lambda: nice_names[attribute]))
 
 
-def overall_parameters(df, baselines=False):
-    sns.set()
-    fig, axes = plt.subplots(nrows=2, ncols=3, sharey=True)
-    for i, attribute in enumerate(attributes):
-        a = df.groupby([attribute, 'collection']).mean()['MAP@10'].unstack()
+def overall_parameters(df, options, baselines=False, split='pretrained'):
+    others = [a for a in attributes if a != split]
+
+    def df_function(collection_df, other):
+        a = group_scores(collection_df, [other, split], **options)
         if baselines:
-            baseline_values = df.where(df[attribute] == baseline_columns[attribute]).groupby('collection').mean()['MAP@10']
+            baseline_values = df.where(df[other] == baseline_columns[other]).groupby('collection').mean()['MAP@10']
             a = (a - baseline_values) / baseline_values
-        current_axes = axes[i // 3, i % 3]
-        a.plot(ax=current_axes)
-        values = a.index.tolist()
-        current_axes.set_xticks(values if all(type(x) is int for x in values) else range(len(values)))
-        current_axes.set_xticklabels(values)
-        current_axes.set_xlabel(nice_names[attribute])
-    plt.show()
+        return a
+
+    plot_per_collection(df, others, df_function, 'Overall paramater impact',
+                        share_x=False, column_labels=nice_names)
 
 
-def impact_of_parameters(df):
-    stds = pd.DataFrame(index=df.groupby('collection').groups, columns=attributes)
+def impact_of_parameters(df, options, split='collection'):
+    stds = pd.DataFrame(index=df.groupby(split).groups, columns=attributes)
     for attribute in attributes:
-        those = df.groupby(['collection', attribute]).mean()['MAP@10'].unstack()
+        those = group_scores(df, [attribute, split], **options).transpose()
         stds[attribute] = (those.max(axis=1) - those.min(axis=1)) / those.min(axis=1)
     return stds
 
