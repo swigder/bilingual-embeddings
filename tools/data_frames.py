049bfde803d2647a06b10527853ca248e9655074
suri <swigder@gmail.com>
2018-05-25 15:01:13 +0200

    plotting the changes rather than performance itself

--- tools/data_frames.py
+++ tools/data_frames.py
@@ -27,6 +27,7 @@ baseline_columns = {'sub': 'No',
 replacements = {'collection': {'ohsu-trec': 'ohsu'},
                 'sub': {'7': 'No'},
                 'pretrained': {True: 'Hybrid', False: 'Collection'}}
+collections = ['adi', 'time', 'ohsu']
 
 
 def load_and_combine(path):
@@ -150,18 +151,51 @@ def group_scores(df, groupby, average, n=2):
             groupby).min()[SCORE].unstack()
 
 
-def plot_per_collection(df, columns, df_function, suptitle, share_x=False, column_labels=None):
+def plot_per_collection_single(df, plot_fn, suptitle, combine_legend=True):
+    sns.set()
+    sns.set_palette(['#66c2a5', '#fc8d62', '#8da0cb'])
+
+    fig = plt.figure(figsize=(9, 3))
+    # fig.suptitle(suptitle, y=.99)
+    axes = fig.subplots(1, 3)
+
+    collection_groups = df.groupby('collection')
+    for i, collection in enumerate(collections):
+        collection_df = collection_groups.get_group(collection)
+        ax = axes[i]
+
+        plot_fn(collection_df, ax)
+        ax.set_title(collection)
+        if ax.get_xlabel() in nice_names:
+            ax.set_xlabel(nice_names[ax.get_xlabel()])
+
+        if not ax.is_first_col():
+            ax.set_ylabel('')
+
+    if combine_legend:
+        for ax in axes[:-1]:
+            ax.legend_.remove()
+        plt.setp(axes[-1].get_legend().get_texts(), fontsize='x-small')
+        plt.setp(axes[-1].get_legend().get_title(), fontsize='x-small')
+        axes[-1].legend_.set_bbox_to_anchor((1, 1.05))
+
+    fig.tight_layout()
+    fig.show()
+
+
+def plot_per_collection(df, columns, plot_fn, suptitle, share_x=False, column_labels=None, x_label=True):
     if not column_labels:
         column_labels = {column: column for column in columns}
 
     sns.set()
+    sns.set_palette(['#66c2a5', '#fc8d62', '#8da0cb'])
 
     fig = plt.figure(figsize=(15, 9))
     fig.suptitle(suptitle)
     outer = gridspec.GridSpec(3, 1, wspace=0.1, hspace=0.1)
 
     collection_groups = df.groupby('collection')
-    for i, collection in enumerate(collection_groups.groups):
+    for i, collection in enumerate(collections):
         collection_df = collection_groups.get_group(collection)
 
         inner = gridspec.GridSpecFromSubplotSpec(1, len(columns), subplot_spec=outer[i], wspace=0.1, hspace=0.1)
@@ -173,19 +207,19 @@ def plot_per_collection(df, columns, df_function, suptitle, share_x=False, colum
             axes[0, j] = ax
 
         for j, column in enumerate(columns):
-            grid = df_function(collection_df, column)
             ax = axes[0, j]
-            grid.plot(ax=ax)
-            values = grid.index.tolist()
-            if ax.is_last_row():
-                ax.set_xticks(values if all(type(x) is int for x in values) else range(len(values)))
-                ax.set_xticklabels(values)
-                if share_x and j != len(columns) // 2:
-                    ax.set_xlabel('')
+            grid = plot_fn(collection_df, column, ax)
+            if x_label:
+                values = grid.index.tolist()
+                if ax.is_last_row():
+                    ax.set_xticks(values if all(type(x) is int for x in values) else range(len(values)))
+                    ax.set_xticklabels(values)
+                    if share_x and j != len(columns) // 2:
+                        ax.set_xlabel('')
+                    else:
+                        ax.set_xlabel(column_labels[column])
                 else:
-                    ax.set_xlabel(column_labels[column])
-            else:
-                plt.setp(ax.get_xticklabels(), visible=False)
+                    plt.setp(ax.get_xticklabels(), visible=False)
 
             if ax.is_first_col():
                 label = collection if i != 1 else 'MAP@10\n\n' + collection
@@ -197,25 +231,101 @@ def plot_per_collection(df, columns, df_function, suptitle, share_x=False, colum
     fig.show()
 
 
-def parameter_interaction(df, options, parameters=attributes):
+def parameter_interaction(df, parameters=attributes, reverse=False):
+    if type(parameters) is str:
+        parameters = [parameters]
     for attribute in parameters:
         others = [a for a in attributes if a != attribute]
 
-        def df_function(collection_df, other):
-            return group_scores(collection_df, [attribute, other], **options)
+        def df_function(collection_df, other, ax):
+            x_attr, hue_attr = (other, attribute) if not reverse else (attribute, other)
+            sns.stripplot(x=x_attr, y=SCORE, hue=hue_attr, data=collection_df,
+                          order=sorted(collection_df[x_attr].unique()),
+                          jitter=0.1, dodge=True, alpha=0.5, ax=ax)
+
+        plot_per_collection(df, others, df_function, nice_names[attribute],
+                            share_x=False, column_labels=defaultdict(lambda: nice_names[attribute]), x_label=False)
+
+
+def single_parameter(df, attribute, split='pretrained'):
+    def df_function(collection_df, ax):
+        sns.stripplot(x=attribute, y=SCORE, hue=split, data=collection_df,
+                      order=sorted(collection_df[attribute].unique()),
+                      jitter=1, dodge=True, alpha=0.5, ax=ax)
+    plot_per_collection_single(df, df_function, nice_names[attribute])
+
+
+def single_parameter_change(df, attribute, split='pretrained', relative=False):
+    new_index = list(set(df.columns).difference([attribute, SCORE]))
+    values = sorted(df[attribute].unique())
+    columns = [(values[i-1], values[i]) for i in range(1, len(values))]
+    column_names = ['{} to {}'.format(i, j) for i, j in columns]
+    DIFF = 'Change in MAP@10'
+    TYPE = 'type'
+
+    def df_function(collection_df, ax):
+        indexed = collection_df.set_index(new_index)
+        difference_dfs = []
+        for (before, after), column_name in zip(columns, column_names):
+            difference_df = pd.DataFrame(index=indexed.index, columns=[DIFF, TYPE])
+            difference_df[DIFF] = (indexed[indexed[attribute] == after][SCORE] - indexed[indexed[attribute] == before][SCORE])
+            if relative:
+                difference_df[DIFF] /= indexed[indexed[attribute] == before][SCORE]
+            difference_df[TYPE] = column_name
+            difference_df.reset_index(inplace=True)
+            difference_dfs.append(difference_df)
+        concatted = pd.concat(difference_dfs)
+        sns.stripplot(x=split, y=DIFF, hue=TYPE, data=concatted,
+                      # order=sorted(collection_df[split].unique()),
+                      jitter=1, dodge=True, alpha=0.2, ax=ax)
+        sns.pointplot(x=split, y=DIFF, hue=TYPE, data=concatted,
+                      # order=sorted(collection_df[attribute].unique()),
+                      dodge=False, join=False, markers='d', legend=False, scale=1, ax=ax)
+        ymin, ymax = concatted[DIFF].min(), concatted[DIFF].max()
+        ymin, ymax = (ymax / -2, ymax) if abs(ymax) > abs(ymin / 2) else (ymin, ymin * -2)
+        ax.set_ylim([ymin * 1.2, ymax * 1.2])
+        handles, labels = ax.get_legend_handles_labels()
+        ax.legend(handles[len(columns):], labels[len(columns):], title=nice_names[attribute])
+
+    plot_per_collection_single(df, df_function, nice_names[attribute], combine_legend=True)
+
+
+def overall_parameters(df, split='pretrained'):
+    others = [a for a in attributes if a != split]
+
+    def df_function(collection_df, attribute, ax):
+        sns.stripplot(x=attribute, y=SCORE, hue=split, data=collection_df,
+                      order=sorted(collection_df[attribute].unique()),
+                      jitter=1, dodge=True, alpha=0.5, ax=ax)
+        # sns.pointplot(x=attribute, y=SCORE, hue=split, data=collection_df,
+        #               order=sorted(collection_df[attribute].unique()),
+        #               dodge=0.01, join=False, markers="d", scale=1, ax=ax)
+
+    plot_per_collection(df, others, df_function, 'Overall paramater impact',
+                        share_x=False, column_labels=nice_names, x_label=False)
+
+
+def parameter_interaction_line(df, options, parameters=attributes):
+    for attribute in parameters:
+        others = [a for a in attributes if a != attribute]
+
+        def df_function(collection_df, other, ax):
+            grid = group_scores(collection_df, [attribute, other], **options)
+            grid.plot(ax=ax)
+            return grid
 
         plot_per_collection(df, others, df_function, nice_names[attribute],
                             share_x=True, column_labels=defaultdict(lambda: nice_names[attribute]))
 
 
-def overall_parameters(df, options, baselines=False, split='pretrained'):
+def overall_parameters_baseline(df, options, split='pretrained'):
     others = [a for a in attributes if a != split]
 
-    def df_function(collection_df, other):
+    def df_function(collection_df, other, ax):
         a = group_scores(collection_df, [other, split], **options)
-        if baselines:
-            baseline_values = df.where(df[other] == baseline_columns[other]).groupby('collection').mean()['MAP@10']
-            a = (a - baseline_values) / baseline_values
+        baseline_values = df.where(df[other] == baseline_columns[other]).groupby('collection').mean()['MAP@10']
+        a = (a - baseline_values) / baseline_values
+        a.plot(ax=ax)
         return a
 
     plot_per_collection(df, others, df_function, 'Overall paramater impact',
